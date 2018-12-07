ba7cda4746c929be7b760c3e66e30cabb1a737a2
Quan Minh Pham <dreamtheater2195@gmail.com>
2018-12-07 23:40:05 +0700

    Sign out button

--- components/Nav.js
+++ components/Nav.js
@@ -1,30 +1,36 @@
+import { Fragment } from "react";
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
+import Signout from "./Signout";
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {({ data }) => {
-        if (data.me) return <p>{data.me.name}</p>;
-        return null;
-      }}
-    </User>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {data.me ? (
+          <Fragment>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+            <Signout />
+          </Fragment>
+        ) : (
+          <Link href="/signup">
+            <a>Sign In</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
