447f52ab54ba8d9286a6fbe98f5b37b2370d93ca
Quan Minh Pham <dreamtheater2195@gmail.com>
2018-12-09 18:02:18 +0700

    Update permission on server

--- components/Permissions.js
+++ components/Permissions.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Query } from "react-apollo";
+import React, { Component, Fragment } from "react";
+import { Query, Mutation } from "react-apollo";
 import Error from "./ErrorMessage";
 import gql from "graphql-tag";
 import Table from "./styles/Table";
@@ -15,6 +15,20 @@ const ALL_USERS_QUERY = gql`
   }
 `;
 
+const UPDATE_PERMISSIONS_MUTATION = gql`
+  mutation UPDATE_PERMISSIONS_MUTATION(
+    $permissions: [Permission]
+    $userId: ID!
+  ) {
+    updatePermissions(permissions: $permissions, userId: $userId) {
+      id
+      permission
+      name
+      email
+    }
+  }
+`;
+
 const POSSIBLE_PERMISSIONS = [
   "ADMIN",
   "USER",
@@ -26,7 +40,6 @@ const POSSIBLE_PERMISSIONS = [
 const Permissions = props => (
   <Query query={ALL_USERS_QUERY}>
     {({ data, loading, error }) => {
-      console.log(data);
       return (
         <div>
           <Error error={error} />
@@ -38,14 +51,14 @@ const Permissions = props => (
                   <th>Name</th>
                   <th>Email</th>
                   {POSSIBLE_PERMISSIONS.map(pms => (
-                    <th>{pms}</th>
+                    <th key={pms}>{pms}</th>
                   ))}
                   <th />
                 </tr>
               </thead>
               <tbody>
                 {data.users.map(user => (
-                  <User user={user} />
+                  <UserPermissions user={user} key={user.id} />
                 ))}
               </tbody>
             </Table>
@@ -56,24 +69,72 @@ const Permissions = props => (
   </Query>
 );
 
-class User extends Component {
+class UserPermissions extends Component {
+  state = {
+    permissions: this.props.user.permission
+  };
+  handlePermissionChange = e => {
+    const checkbox = e.target;
+    let updatedPermissions = [...this.state.permissions];
+    if (checkbox.checked) {
+      updatedPermissions.push(checkbox.value);
+    } else {
+      updatedPermissions = updatedPermissions.filter(
+        pms => pms !== checkbox.value
+      );
+    }
+    this.setState({
+      permissions: updatedPermissions
+    });
+  };
   render() {
     const { user } = this.props;
     return (
-      <tr>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        {POSSIBLE_PERMISSIONS.map(pms => (
-          <td>
-            <label htmlFor={`${user.id}-permission-${pms}`}>
-              <input type="checkbox" />
-            </label>
-          </td>
-        ))}
-        <td>
-          <SickButton>Update</SickButton>
-        </td>
-      </tr>
+      <Mutation
+        mutation={UPDATE_PERMISSIONS_MUTATION}
+        variables={{
+          permissions: this.state.permissions,
+          userId: user.id
+        }}
+      >
+        {(updatePermissions, { loading, error }) => (
+          <Fragment>
+            {error && (
+              <tr>
+                <td colSpan={8}>
+                  <Error error={error} />
+                </td>
+              </tr>
+            )}
+            <tr>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              {POSSIBLE_PERMISSIONS.map(pms => (
+                <td key={pms}>
+                  <label htmlFor={`${user.id}-permission-${pms}`}>
+                    <input
+                      id={`${user.id}-permission-${pms}`}
+                      type="checkbox"
+                      checked={this.state.permissions.includes(pms)}
+                      value={pms}
+                      onChange={this.handlePermissionChange}
+                    />
+                  </label>
+                </td>
+              ))}
+              <td>
+                <SickButton
+                  onClick={updatePermissions}
+                  disabled={loading}
+                  type="button"
+                >
+                  {loading ? "Updating" : "Update"}
+                </SickButton>
+              </td>
+            </tr>
+          </Fragment>
+        )}
+      </Mutation>
     );
   }
 }
